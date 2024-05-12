a2b24812f3100ccb2adeb4135cfbab7890d263c9
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-05-13 00:20:17 +0900

    feat: get filtered items

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,38 @@ var router = express.Router();
 
 const Product = require('../models/product');
 
+const types = ['FOOTWEAR', 'APPAREL'];
+const brands = ['NIKE', 'ADIDAS', 'NEWBALANCE'];
+
 router.get('/products', async function (req, res, next) {
   try {
-    const category = req.query.category;
-    const type = req.query.type;
-    const brand = req.query.brand;
+    const selectedTypes = req.body.type;
+    const selectedBrands = req.body.brand;
+    const selectedSortby = req.body.sortby;
 
-    let products = await Product.find({});
+    let filterdProducts = await Product.find({});
 
-    res.status(200).send(products);
-  } catch (err) {}
+    if (selectedTypes) {
+      filterdProducts = filterdProducts.filter((product) =>
+        selectedTypes.includes(product.type)
+      );
+    }
+    if (selectedBrands) {
+      filterdProducts = filterdProducts.filter((product) =>
+        selectedBrands.includes(product.brand)
+      );
+    }
+
+    if (filterdProducts.length == 0) {
+      res.status(204).send();
+    }
+
+    //sort
+
+    res.status(200).send(filterdProducts);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 module.exports = router;
